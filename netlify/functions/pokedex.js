7142f5ef1cb2cf66a8ba620c56bc32ab1d3d8807
Tarun Balaji Ravi <tarunbalaji3@gmail.com>
2022-04-12 17:17:49 -0400

    Add console.log() statements

--- netlify/functions/pokedex.js
+++ netlify/functions/pokedex.js
@@ -2,12 +2,22 @@
 const fetch = (...args) =>
   import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
+const chalk = require('chalk');
+const { DateTime } = require('luxon');
+
 exports.handler = async function (event, context) {
+  const date = DateTime.now();
   const eventBody = JSON.parse(event.body);
+  console.log(chalk.yellow(`${date}`));
+  console.log(chalk.cyan(`\teventBody.region: ${eventBody.region}`));
   const POKE_API = 'https://pokeapi.co/api/v2/pokedex/' + eventBody.region;
 
   const response = await fetch(POKE_API);
   const data = await response.json();
+
+  console.log(
+    chalk.redBright(`\tNumber of entries: ${data.pokemon_entries.length}`)
+  );
 
   return {
     statusCode: 200,
